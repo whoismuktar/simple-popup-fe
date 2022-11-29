478435eaabdf9f4ef9d221b824eea192c9cf5504
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-29 23:35:36 +0100

    active popup progress

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
+    <ActivePopup></ActivePopup>
   </div>
 </template>
 
 <script>
+import ActivePopup from "@/components/PopUps/ActivePopup"
 
 export default {
   name: "App",
+  components: { ActivePopup },
 }
 </script>
 
---
+++ src/components/PopUps/ActivePopup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="popup popup__default">
+    <div class="popup-wrapper">
+      <div class="popup-inner">
+        <div class="popup-inner__wrapper">
+          <div class="popup-item popup-stars"></div>
+          <div class="popup-item popup-text">
+            All the text and elements in this popup should be editable and dragable
+          </div>
+          <div class="popup-item popup-input">
+            <input type="email" placeholder="E-mail" />
+          </div>
+          <div class="popup-item popup-cta">
+            <button>Signup Now</button>
+          </div>
+          <div class="popup-item popup-footnote">
+            No credit card required. No Surprises
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="@/assets/styles/popup.scss" lang="scss" scoped></style>
