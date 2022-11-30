945807bd5d3e9ab69a3d62247bc67870e0b372e4
Muktar Yusuf <mayunited12@yahoo.com>
2022-11-30 13:45:25 +0100

    improvee rendered popup

--- src/components/PopUps/RenderPopup.vue
+++ src/components/PopUps/RenderPopup.vue
@@ -5,14 +5,23 @@
 </template>
 
 <script>
-import ActivePopup from "./ActivePopup"
+import ActivePopup from "./ActivePopup";
 
 export default {
   components: { ActivePopup },
-
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.render-active-popup {
+  position: fixed;
+  width: 500px;
+  height: 200px;
+  background-color: #fff;
+  padding: 10px;
+  z-index: 200;
+  margin: 5% auto;
+  left: 0;
+  right: 0;
+}
 </style>
