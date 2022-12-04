b20c4ddcb2b12f3c0e6851175556cf71c6d8d127
Muktar Yusuf <mayunited12@yahoo.com>
2022-12-04 21:58:11 +0100

    implment embedding entry point for production env

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
-import Vue from "vue"
-import App from "./App.vue"
-import store from "./store"
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import router from "./router"
-import { DraggablePlugin } from '@braks/revue-draggable';
+import router from "./router";
+import { DraggablePlugin } from "@braks/revue-draggable";
 
+Vue.config.productionTip = false;
+Vue.use(DraggablePlugin);
 
-Vue.config.productionTip = false
-Vue.use(DraggablePlugin)
+// Environment dependent mounting
+let tag = "";
+process.env.NODE_ENV === "development" ? (tag = "#app") : (tag = "#emPop");
 
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount("#app")
+  render: (h) => h(App),
+}).$mount(tag);
